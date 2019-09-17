ac1ac502d51f0bf54b8315bf9749dd6780df597f
VieyKer <55123502+VieyKer@users.noreply.github.com>
2019-09-17 14:59:49 +0300

    template + ex1, ex4

---
+++ BinaryTreeProject/BinaryTree.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "BinaryTree.h"
+
+
+int emptyTree(Tree *newtree)
+{
+	if (newtree == NULL)
+		return 0;
+	return 1;
+}
+
+int checkLeaf(Tree *newtree)
+{
+	if (!emptyTree(newtree))
+		return 0;
+	return (newtree->LeftSon == NULL && newtree->RightSon == NULL);
+}
+
+Tree *leftSon(Tree *newtree)
+{
+	if (!emptyTree(newtree))
+		return 0;
+	return newtree->LeftSon;
+}
+
+Tree *rightSon(Tree *newtree)
+{
+	if (!emptyTree(newtree))
+		return 0;
+	return newtree->RightSon;
+}
+
+Tree *parent (Tree *newtree)
+{
+	if (!emptyTree(newtree))
+		return 0;
+	return newtree->Parent;
+}
+
+tree_type nodeData (Tree *newtree)
+{
+	if (!emptyTree(newtree))
+		return 0;
+	return newtree->value;
+}
+
+void insertToTree(Tree **Root, Tree *parent, tree_type data)
+{
+	Tree *temp = NULL;
+
+	if ((*Root) == NULL)
+	{
+		temp = (Tree *)malloc(sizeof(Tree));
+		temp->LeftSon = temp->RightSon = NULL;
+		temp->Parent = parent;
+		temp->value = data;
+		(*Root) = temp;
+		return;
+	}
+
+	if (data > (*Root)->value)
+	{
+		insertToTree(&(*Root)->RightSon, *Root, data);
+	}
+	else if (data < (*Root)->value)
+	{
+		insertToTree(&(*Root)->LeftSon, *Root, data);
+	}
+}
+
+void deleteTree(Tree *deltree)
+{
+	if (deltree)
+	{
+		deleteTree(deltree->RightSon);
+		deleteTree(deltree->LeftSon);
+		free(deltree);
+	}
+	
+}
+
+void printInorder(Tree *printTree)
+{
+
+}
---
+++ BinaryTreeProject/BinaryTree.h
@@ -0,0 +1,20 @@
+#pragma once
+
+typedef int tree_type ;
+
+typedef struct Tree
+{
+	tree_type value;
+	struct Tree *Parent;
+	struct Tree *LeftSon;
+	struct Tree *RightSon;
+}Tree;
+
+int emptyTree(Tree *newtree);
+int checkLeaf(Tree *newtree);
+Tree *leftSon(Tree *newtree);
+Tree *rightSon(Tree *newtree);
+Tree *parent(Tree *newtree);
+tree_type nodeData(Tree *newtree);
+
+void insertToTree(Tree **Root, Tree *parent, tree_type data);
